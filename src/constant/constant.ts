18b638023b1a4390cacda696d169a83ef3bbbc56
Ashish <rohitmerawat807@gmail.com>
2025-02-08 10:44:33 +0530

    [101 onboarding screen designed]

---
+++ src/constant/constant.ts
@@ -0,0 +1,35 @@
+
+export const COLORS = {
+    primary:"#009B17",
+    white:"#ffffff"
+}
+
+export const onboardingData = 
+    [
+        {
+            image:require("../assets/images/one.png"),
+            title:"Welcome to Laundry Service",
+            subtitle:"Here are simple examples that can be edited in real time in your browser:",
+        },
+        {
+            image:require("../assets/images/two.png"),
+            title:"Need something?",
+            subtitle:"Use Laundry Service to find and rent nearby in just a snap!",
+        },
+        {
+            image:require("../assets/images/three.png"),
+            title:"Turn liability is to assets!",
+            subtitle:"List your stuff for free on Laundry Service and start earning today!",
+        }
+]
+
+export const IMAGES = {
+    right_arrow:require("../assets/images/right_arrow.png"),
+    onboarding_background:require("../assets/images/onboarding_background.png"),
+    one:require("../assets/images/one.png"),
+    two:require("../assets/images/two.png"),
+    three:require("../assets/images/three.png"),
+    onboarding_background2:require("../assets/images/onboarding_background2.png"),
+    onboarding_background3:require("../assets/images/onboarding_background3.png")
+}
+
